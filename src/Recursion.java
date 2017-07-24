a9e229d6143bf6f683bc5b5ce23c9bb57da1daaa
abdul-kader138 <babu313136@gmail.com>
2017-07-24 17:04:45 +0600

    Recursion Example

--- src/Recursion.java
+++ src/Recursion.java
@@ -13,7 +13,7 @@ public class Recursion {
     private static String reverseString(String args) {
         if (args == null || args.length() < 1 || args.length() == 1) return args;
         else{
-        return args.charAt(args.length() - 1) + reverseString(args.substring(0,args.length() - 1));
+        return args.charAt(args.length()-1) + reverseString(args.substring(0,args.length() - 1));
         }
     }
 }
