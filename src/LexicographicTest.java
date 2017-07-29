021c1749a5cf34cd3a6c0e8c30b954c7d41c82de
abdul-kader138 <babu313136@gmail.com>
2017-07-29 14:24:05 +0600

    Lexicographic test using java

--- src/LexicographicTest.java
+++ src/LexicographicTest.java
@@ -1,13 +1,16 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
 
 /**
  * Developer: Abdul Kader
  * Date- 07/29/2017.
  **/
-public class LexicographicExample {
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class LexicographicTest {
     public static void main(String a[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String:-");
@@ -15,6 +18,13 @@ public class LexicographicExample {
         System.out.println("Enter the length");
         int count = sc.nextInt();
         List<String> list = new ArrayList<String>();
+
+        /*
+        * (str.length() - i >= count) we check until length small then count for per fetch
+        * and substring to current index to count+current index
+        * then add substring value to list
+        */
+
         for (int i = 0; i < str.length(); i = i + 1) {
             if (str.length() - i >= count) {
                 list.add(str.substring(i, count + i));
