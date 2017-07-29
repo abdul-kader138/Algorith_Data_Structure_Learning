55eb7e3f1d47334f8b8dbfb31bdcaaba8b218f26
abdul-kader138 <babu313136@gmail.com>
2017-07-29 14:08:51 +0600

    Lexicographic test using java

---
+++ src/LexicographicExample.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Developer: Abdul Kader
+ * Date- 07/29/2017.
+ **/
+public class LexicographicExample {
+    public static void main(String a[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String:-");
+        String str = sc.nextLine();
+        System.out.println("Enter the length");
+        int count = sc.nextInt();
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < str.length(); i = i + 1) {
+            if (str.length() - i >= count) {
+                list.add(str.substring(i, count + i));
+            }
+        }
+        Collections.sort(list);
+        System.out.println("Smallest subString:-" + list.get(0));
+        System.out.println("Largest subString:-" + list.get(list.size() - 1));
+    }
+}
