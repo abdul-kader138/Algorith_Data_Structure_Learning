2032f54ce728e1e6501beb675653a101ddb06047
abdul-kader138 <babu313136@gmail.com>
2017-08-01 15:29:25 +0600

    Lambda (Java 8 new features Testing)

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -177,7 +177,10 @@ public class LambdaTest {
 
 
 
-        /* Stream interface and Map */
+        /* Stream interface and match */
+        /*
+        * Various matching operations can be used to check whether a certain predicate matches the stream.
+        * */
         System.out.println(empList.stream().anyMatch((p) -> p.getName().startsWith("1")));
 
 
