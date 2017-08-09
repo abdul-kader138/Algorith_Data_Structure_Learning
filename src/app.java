c3adb62c3b7eb2c3a597b50566aeb101330468c1
abdul-kader138 <babu313136@gmail.com>
2017-08-09 14:55:00 +0600

    Hacker Rank problem solving-List

--- src/app.java
+++ src/app.java
@@ -12,36 +12,39 @@ import java.util.ArrayList;
 public class app {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Scanner s1 = new Scanner(System.in);
-        Scanner br = new Scanner(System.in);
-        int index = br.nextInt();
-        String stringArray = s1.nextLine();
+        int index = in.nextInt();
+        in.nextLine();
+        String stringArray = in.nextLine();
         int n = in.nextInt();
         in.nextLine();
-        List<Integer> list=new ArrayList<Integer>(index);
-        List<Integer> finalList=new ArrayList<Integer>(index);
+        List<Integer> list = new ArrayList<Integer>(index);
+        List<Integer> finalList = new ArrayList<Integer>(index);
+
+
         // add list value
         if (!stringArray.equals("")) {
             String[] listVal = stringArray.split(" ");
-            finalList=new ArrayList<Integer>(index);
+            finalList = new ArrayList<Integer>(index);
             for (int i = 0; i < index; i++) {
                 list.add(Integer.parseInt(listVal[i]));
             }
         }
 
 
-        for (int i = 0; i < n; i++) {
-            String value = in.nextLine();
+        for (int z = 0; z < n; z++) {
+            String value = in.next();
+            in.nextLine();
             String valueList = in.nextLine();
-            String[] insertIndex = valueList.split(" ");
-            int[] insertIndexVal = new int[insertIndex.length];
-            for (int q = 0; q < insertIndex.length; q++) {
-                insertIndexVal[q] = Integer.parseInt(insertIndex[q]);
-            }
-            if(value.equals("Insert")) finalList=addToArray(list,insertIndexVal);
-            if(value.equals("Delete")) finalList=removeToArray(list, insertIndexVal);
+            if (!valueList.equals("")) {
+                    String[] insertIndex = valueList.split(" ");
+                    int[] insertIndexVal = new int[insertIndex.length];
+                    for (int q = 0; q < insertIndex.length; q++) {
+                        insertIndexVal[q] = Integer.parseInt(insertIndex[q]);
+                    }
+                    if (value.equals("Insert")) finalList = addToArray(list, insertIndexVal);
+                    if (value.equals("Delete")) finalList = removeToArray(list, insertIndexVal);
+                }
         }
-
 
         String finalVal = "";
         if (finalList.size() > 0) {
@@ -51,15 +54,15 @@ public class app {
         }
         System.out.println(finalVal.trim());
 
+
     }
 
-
-    private static List<Integer> addToArray(List<Integer> list, int[] indexVal){
-        list.add(indexVal[0],indexVal[1]);
+    private static List<Integer> addToArray(List<Integer> list, int[] indexVal) {
+        list.add(indexVal[0], indexVal[1]);
         return list;
     }
 
-    private static List<Integer> removeToArray(List<Integer> list, int[] indexVal){
+    private static List<Integer> removeToArray(List<Integer> list, int[] indexVal) {
         list.remove(indexVal[0]);
         return list;
     }
