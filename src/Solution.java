d6f7a5c18adbf1f5bb9856344b75a9f44bc1746a
abdul-kader138 <babu313136@gmail.com>
2017-08-09 11:23:32 +0600

    Solve Hacker Rank big integer problem

--- src/Solution.java
+++ src/Solution.java
@@ -19,6 +19,22 @@ public class Solution {
         int arrayLength=s.length-2;
 
 
+
+
+        String[] s2=new String[n];
+
+        for(int w=0; w<n;w++) s2[w]=s[w];
+
+        Arrays.sort(s2, Collections.reverseOrder(new Comparator<String>() {
+            @Override
+            public int compare(String a1, String a2) {
+                BigDecimal a = new BigDecimal(a1);
+                BigDecimal b = new BigDecimal(a2);
+                return a.compareTo(b);
+            }
+        }));
+
+        for(int w=0; w<n;w++) s[w]=s2[w];
         //Output
         for (int i = 0; i < n; i++) {
             System.out.println(s[i]);
