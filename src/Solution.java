9d99bdab8a24ba3b7025f124c88481bc56c3f9aa
abdul-kader138 <babu313136@gmail.com>
2017-08-10 13:08:15 +0600

    Hacker Rank problem solving-Hashing

--- src/Solution.java
+++ src/Solution.java
@@ -6,39 +6,7 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) throws Exception{
-        Scanner sn = new Scanner(System.in);
-        String data = sn.nextLine();
-
-        String hash = getMD5Hash(data);
-        System.out.println(hash.toLowerCase());
-    }
-
-    /**
-     * Returns a hexadecimal encoded MD5 hash for the input String.
-     * @param data
-     * @return
-     */
-    private static String getMD5Hash(String data) {
-        String result = null;
-        try {
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-            byte[] hash = digest.digest(data.getBytes("UTF-8"));
-            return bytesToHex(hash); // make it printable
-        }catch(Exception ex) {
-            ex.printStackTrace();
-        }
-        return result;
-    }
-
-    /**
-     * Use javax.xml.bind.DatatypeConverter class in JDK to convert byte array
-     * to a hexadecimal string. Note that this generates hexadecimal in upper case.
-     * @param hash
-     * @return
-     */
-    private static String  bytesToHex(byte[] hash) {
-        return DatatypeConverter.printHexBinary(hash);
+    public static void main(String[] args) throws Exception {
     }
 }
 
