2de85ee3b78a29240c404811d3a01486ad3ca954
abdul-kader138 <babu313136@gmail.com>
2017-08-10 14:26:24 +0600

    Code Refactoring

---
+++ src/Lambda/Annotation_Example/FamilyMember.java
@@ -0,0 +1,21 @@
+package Lambda.Annotation_Example;
+
+
+public class FamilyMember {
+
+
+    @FamilyBudget(budgetLimit = 100,userRole = "SENIOR")
+    public void seniorMember(int budget, int moneySpend) {
+        System.out.println("Senior Member");
+        System.out.println("Spend: " + moneySpend);
+        System.out.println("Budget Left: " + (budget - moneySpend));
+    }
+
+
+    @FamilyBudget(budgetLimit = 50,userRole = "JUNIOR")
+    public void juniorUser(int budget, int moneySpend) {
+        System.out.println("Junior Member");
+        System.out.println("Spend: " + moneySpend);
+        System.out.println("Budget Left: " + (budget - moneySpend));
+    }
+}
