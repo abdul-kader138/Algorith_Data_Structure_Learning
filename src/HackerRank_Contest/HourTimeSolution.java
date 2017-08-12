82eab584fbaf64f0d34ea892766ced1ac629b2b1
abdul-kader138 <babu313136@gmail.com>
2017-08-12 11:50:51 +0600

    Hacker Rank Contest-Time problem Solution

--- src/HackerRank_Contest/HourTimeSolution.java
+++ src/HackerRank_Contest/HourTimeSolution.java
@@ -1,5 +1,9 @@
 package HackerRank_Contest;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -9,7 +13,20 @@ public class HourTimeSolution {
 
 
     static String timeConversion(String s) {
-        return new String();
+        String s1=s.substring(0,8);
+        String s2=s.substring(8);
+        String input = s1+" "+s2;
+        DateFormat df = new SimpleDateFormat("hh:mm:ss aa");
+        DateFormat outputFormat = new SimpleDateFormat("HH:mm:ss");
+        Date date = null;
+        String output = null;
+        try{
+            date= df.parse(input);
+            output = outputFormat.format(date);
+        }catch(ParseException pe){
+            pe.printStackTrace();
+        }
+        return output;
     }
 
     public static void main(String[] args) {
