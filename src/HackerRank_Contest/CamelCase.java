a826200bacdec20b42ea7851e66dca46c6a43331
abdul-kader138 <babu313136@gmail.com>
2017-08-12 15:45:25 +0600

    Hacker Rank Contest-Camel Case

--- src/HackerRank_Contest/CamelCase.java
+++ src/HackerRank_Contest/CamelCase.java
@@ -1,7 +1,15 @@
 package HackerRank_Contest;
 
-/**
- * Created by a.kader on 8/12/2017.
- */
+
+import java.util.Scanner;
+
 public class CamelCase {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+
+        String[] x = s.split("(?=[A-Z])");
+        System.out.println(x.length);
+    }
 }
