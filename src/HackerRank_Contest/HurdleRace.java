2b6957631e93d4887217653852f585050d233acc
abdul-kader138 <babu313136@gmail.com>
2017-08-12 13:57:15 +0600

    Hacker Rank Contest-Hurdle Race

--- src/HackerRank_Contest/HurdleRace.java
+++ src/HackerRank_Contest/HurdleRace.java
@@ -1,5 +1,27 @@
 package HackerRank_Contest;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class HurdleRace {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] height = new int[n];
+        List<Integer> numberList = new ArrayList<>();
+        for (int height_i = 0; height_i < n; height_i++) {
+            height[height_i] = in.nextInt();
+        }
+
+        for(int val:height) numberList.add(val);
+        int maxNum = Collections.max(numberList);
+        int result = 0;
+        if (maxNum > k) result = maxNum - k;
+        System.out.println(result);
+    }
 }
