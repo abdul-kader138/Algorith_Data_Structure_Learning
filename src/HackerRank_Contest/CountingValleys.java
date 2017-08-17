b60ed1c886e183496038346b8b59431ad41866ac
abdul-kader138 <babu313136@gmail.com>
2017-08-17 13:29:21 +0600

    Hacker Rank Contest-Counting Valleys

--- src/HackerRank_Contest/CountingValleys.java
+++ src/HackerRank_Contest/CountingValleys.java
@@ -1,7 +1,35 @@
 package HackerRank_Contest;
 
-/**
- * Created by a.kader on 8/17/2017.
- */
+
+import java.util.Scanner;
+
 public class CountingValleys {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String s = in.next();
+        System.out.println(countMountain(n, s));
+    }
+
+
+    public static int countMountain(int n, String s) {
+        int level = 0;
+        int result = 0;
+        int up = 0;
+        int down = 0;
+        char[] values = s.toCharArray();
+        for (int i = 0; i < values.length; i++) {
+            if ('U' == values[i]) up += 1;
+            if ('D' == values[i]) down += 1;
+            if (level == (up - down) && up > 3 && down > 3) {
+                result += 1;
+                up = 0;
+                down = 0;
+            }
+        }
+
+        return result;
+    }
+
 }
