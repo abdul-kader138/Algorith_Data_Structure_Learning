05013da03ae6c15f7d8d8d394d48cc3810c452fe
abdul-kader138 <babu313136@gmail.com>
2017-08-21 15:25:45 +0600

    Java 8 features Testing

---
+++ src/Lambda/App.java
@@ -0,0 +1,40 @@
+package Lambda;
+
+import java.util.Stack;
+
+/**
+ * Created by a.kader on 8/21/2017.
+ */
+public class App {
+
+    public static void main(String [] args){
+
+
+        Employee employee=new Employee();
+
+        employee.setAge(30);
+        employee.setName("Abdul Kader");
+        employee.setSalary(40000);
+
+        Employee employee1=new Employee();
+        employee1.setAge(30);
+        employee1.setName("Boss-1");
+        employee1.setSalary(30000);
+
+        Employee employee2=new Employee();
+        employee2.setAge(40);
+        employee2.setName("Boss-2");
+        employee2.setSalary(50000);
+
+        Stack<Employee> stack=new Stack<>();
+        stack.add(employee);
+        stack.add(employee1);
+        stack.add(employee2);
+
+
+        double totalSalary=stack.stream().filter((t) -> t.getAge() > 29).mapToDouble(t -> t.getSalary()).sum();
+        System.out.println(totalSalary);
+
+
+    }
+}
