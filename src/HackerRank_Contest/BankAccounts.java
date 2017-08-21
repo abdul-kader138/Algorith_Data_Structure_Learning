cac7d7a953bbf556f330c8a27c750eebf7677f42
abdul-kader138 <babu313136@gmail.com>
2017-08-21 15:03:37 +0600

    Hacker Rank Contest-Bank Accounts

--- src/HackerRank_Contest/BankAccounts.java
+++ src/HackerRank_Contest/BankAccounts.java
@@ -1,10 +1,7 @@
 package HackerRank_Contest;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class BankAccounts {
     static String feeOrUpfront(int n, int k, int x, int d, int[] p) {
@@ -25,19 +22,34 @@ public class BankAccounts {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int q = in.nextInt();
-        for (int a0 = 0; a0 < q; a0++) {
-            int n = in.nextInt();
-            int k = in.nextInt();
-            int x = in.nextInt();
-            int d = in.nextInt();
-            int[] p = new int[n];
-            for (int p_i = 0; p_i < n; p_i++) {
-                p[p_i] = in.nextInt();
-            }
-            String result = feeOrUpfront(n, k, x, d, p);
-            System.out.println(result);
-        }
+//        int q = in.nextInt();
+//        for (int a0 = 0; a0 < q; a0++) {
+//            int n = in.nextInt();
+//            int k = in.nextInt();
+//            int x = in.nextInt();
+//            int d = in.nextInt();
+//            int[] p = new int[n];
+//            for (int p_i = 0; p_i < n; p_i++) {
+//                p[p_i] = in.nextInt();
+//            }
+//            String result = feeOrUpfront(n, k, x, d, p);
+//            System.out.println(result);
+//        }
+
+        Queue<Integer> queue=new LinkedList<>();
+
+        queue.add(11);
+        queue.add(12);
+        queue.add(13);
+        queue.add(14);
+        queue.add(15);
+        queue.add(16);
+        queue.add(17);
+
+        System.out.println(queue.peek());
+//        System.out.println(queue.poll());
+
+        for(int i:queue) System.out.println(i);
         in.close();
     }
 
