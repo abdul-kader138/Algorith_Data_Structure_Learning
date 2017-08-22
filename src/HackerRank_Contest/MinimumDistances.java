8c5f5c60abc6f508ba35e951d5acd6048e8bf1b8
abdul-kader138 <babu313136@gmail.com>
2017-08-22 13:10:05 +0600

    Hacker Rank -Minimum Distances

--- src/HackerRank_Contest/MinimumDistances.java
+++ src/HackerRank_Contest/MinimumDistances.java
@@ -1,7 +1,7 @@
 package HackerRank_Contest;
 
 
-import java.util.Scanner;
+import java.util.*;
 
 public class MinimumDistances {
 
@@ -9,9 +9,40 @@ public class MinimumDistances {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int A[] = new int[n];
-        for(int A_i=0; A_i < n; A_i++){
+        Integer A[] = new Integer[n];
+        for (int A_i = 0; A_i < n; A_i++) {
             A[A_i] = in.nextInt();
         }
+        getValue(A);
+    }
+
+
+    private static void getValue(Integer[] obj) {
+        Map<Integer, Integer> valueList = new HashMap<>();
+
+        for (int i = 0; i < obj.length; i++) {
+            for (int j = 0; j < obj.length; j++) {
+                int previousValue = 0;
+                if (i != j) {
+                    if (valueList.size() > 0) previousValue = valueList.get(obj[j]);
+                    if (obj[i] == obj[j] && previousValue < j && previousValue >-1) {
+                        valueList.put(obj[j], j);
+                    }
+                }
+            }
+        }
+
+        List<Integer> indexList = Arrays.asList(obj);
+        List<Integer> minValObj = new ArrayList<>();
+
+        for (Map.Entry val : valueList.entrySet()) {
+            int index = indexList.indexOf((int) val.getKey());
+            minValObj.add(((int) val.getValue() - index));
+        }
+
+        if (minValObj.size() == 0) System.out.println(-1);
+        else System.out.println(Collections.min(minValObj));
+
+
     }
 }
