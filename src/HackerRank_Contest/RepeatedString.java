4a82b25baf48375233b6f69d50d1bfd1a2b0a992
abdul-kader138 <babu313136@gmail.com>
2017-08-27 15:22:01 +0600

    Hacker Rank - Repeated String

--- src/HackerRank_Contest/RepeatedString.java
+++ src/HackerRank_Contest/RepeatedString.java
@@ -14,14 +14,22 @@ public class RepeatedString {
         Pattern p = Pattern.compile("a");
         long count = 0;
         long count2 = 0;
+
+        /* Find number of match a from given String */
         Matcher m = p.matcher(s);
         while (m.find()) count += 1;
+
+        /* Calculate how many time big output will create from given string */
         long wordLen=(long)s.length();
         long divisor=(n/wordLen);
         long totalDiff=(n-(divisor*wordLen));
+
+        /* If need to spilt then substring main string */
         String subLength=s.substring(0,(int) totalDiff);
         Matcher m1 = p.matcher(subLength);
         while (m1.find()) count2 += 1;
+
+        /* Final output  */
         long result=((count*divisor)+count2);
         System.out.println(result);
 
