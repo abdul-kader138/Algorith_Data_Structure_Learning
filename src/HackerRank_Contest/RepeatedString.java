a37bcfbdd57d33a6222cf7c182c8274d1bffc5c3
abdul-kader138 <babu313136@gmail.com>
2017-08-27 09:45:43 +0600

    Hacker Rank - Repeated String

---
+++ src/HackerRank_Contest/RepeatedString.java
@@ -0,0 +1,13 @@
+package HackerRank_Contest;
+
+/**
+ * Created by a.kader on 8/27/2017.
+ */
+public class RepeatedString {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        long n = in.nextLong();
+    }
+}
