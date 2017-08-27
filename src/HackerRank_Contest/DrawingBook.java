c010f39d8a39bb13717dc673db300393decabc60
abdul-kader138 <babu313136@gmail.com>
2017-08-27 16:13:04 +0600

    Hacker Rank - Drawing Book

--- src/HackerRank_Contest/DrawingBook.java
+++ src/HackerRank_Contest/DrawingBook.java
@@ -1,6 +1,20 @@
 package HackerRank_Contest;
 
 
+import java.util.Scanner;
+
 public class DrawingBook {
 
+
+    static int solve(int n, int p){
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int p = in.nextInt();
+        int result = solve(n, p);
+        System.out.println(result);
+    }
 }
