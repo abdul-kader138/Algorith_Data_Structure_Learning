9d2538acb71e625f191e268ad80256987bfae1a8
abdul-kader138 <babu313136@gmail.com>
2017-08-28 10:53:39 +0600

    Hacker Rank - Drawing Book(Partial)

--- src/HackerRank_Contest/DrawingBook.java
+++ src/HackerRank_Contest/DrawingBook.java
@@ -5,14 +5,39 @@ import java.util.Scanner;
 
 public class DrawingBook {
 
-
     static int solve(int n, int p) {
-        if(n==p) return 0;
-        int firstForward = p - 1;
-        int backWard = ((n - p)-1);
+        int firstForward = 0;
+        int backWard = 0;
+        boolean even=isEven(p);
+        if(even){
+            for(int i=2;i<=p;i+=2){
+                firstForward+=1;
+            }
+            for(int i=p;i>=p;i-=2){
+                backWard+=1;
+            }
+        }
+        else{
+            for(int i=1;i<=p;i+=2){
+                firstForward+=1;
+            }
+            for(int i=p;i>=p;i-=1){
+                backWard+=1;
+            }
+        }
+
+        if (n == p) return 0;
         if (firstForward < backWard) return firstForward;
         else return backWard;
     }
+
+
+    private static boolean isEven(int num) {
+        boolean even = true;
+        if (num % 2 == 0) return even;
+        else return false;
+    }
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
