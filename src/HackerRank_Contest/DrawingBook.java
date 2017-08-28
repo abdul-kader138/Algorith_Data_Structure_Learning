d50a5607ef573e22c042840023b5302c23f45472
abdul-kader138 <babu313136@gmail.com>
2017-08-28 10:09:09 +0600

    Hacker Rank - Drawing Book(Partial)

--- src/HackerRank_Contest/DrawingBook.java
+++ src/HackerRank_Contest/DrawingBook.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 public class DrawingBook {
 
 
-    static int solve(int n, int p){
-        // Complete this function
-//        int first=p-1;
-//
-//        int last=n-p;
-//        if(first<last) return first;
-        return 1;
+    static int solve(int n, int p) {
+
+        int firstForward = p - 1;
+
+        int backWard = n - p;
+
+        if (firstForward < backWard) return firstForward;
+        else return backWard;
     }
 
     public static void main(String[] args) {
