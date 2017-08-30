e7be87b0bf0446819d073bba25702416eefda1d6
abdul-kader138 <babu313136@gmail.com>
2017-08-30 14:52:39 +0600

    Hacker Rank -Marc's Cakewalk(Partial)

--- src/HackerRank_Contest/MarcsCakewalk.java
+++ src/HackerRank_Contest/MarcsCakewalk.java
@@ -1,5 +1,25 @@
 package HackerRank_Contest;
 
 
+import java.util.Scanner;
+
 public class MarcsCakewalk {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] calories = new int[n];
+        for (int calories_i = 0; calories_i < n; calories_i++) {
+            calories[calories_i] = in.nextInt();
+        }
+
+        double currentResult=0;
+        int startNum=0;
+        for(int j=0;j<calories.length;j++){
+            if(j==0) currentResult = (0+((n-j)*Math.pow(2,j)));
+            else currentResult=(currentResult+((n-j)*Math.pow(2,j)));
+        }
+
+        System.out.println(currentResult);
+    }
 }
