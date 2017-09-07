31caf34c7393f7534dfab237e8ac40c6b1b2646a
abdul-kader138 <babu313136@gmail.com>
2017-09-07 13:41:34 +0600

    Java 8- New Features Implementation(Consumer)

--- src/Lambda/App.java
+++ src/Lambda/App.java
@@ -1,23 +1,24 @@
 package Lambda;
 
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 
 public class App {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
 
-
-        String name="Hello World";
+        String name = "Hello World";
 
         /*
         * Params: String, Interface
         * Here we used the power of Lambda and implementing interface using Lambda
         * */
 
-         MethodReferenceTest methodReferenceTest=new MethodReferenceTest();
-        methodReferenceTest.getResult(name,(val)->{
+        MethodReferenceTest methodReferenceTest = new MethodReferenceTest();
+        methodReferenceTest.getResult(name, (val) -> {
             return val.toUpperCase();
         });
 
@@ -27,7 +28,37 @@ public class App {
         * Here we used Method reference of String Parser Class
         * */
 
-        StringParser stringParser=new StringParser();
-        methodReferenceTest.getResult(name,stringParser::print);
+        StringParser stringParser = new StringParser();
+        methodReferenceTest.getResult(name, stringParser::print);
+
+
+       /*
+        * Params: String, Interface
+        * Here we used Method reference of String Parser Class
+        * */
+
+
+        Consumer<Emp> consumer = (obj) -> {
+            if (obj.getName().length() <= 3) System.out.println(obj.getName().toLowerCase());
+            else System.out.println(obj.getName().toUpperCase());
+        };
+
+        Emp e = new Emp();
+        e.setName("Abdul Kader");
+        e.setAge(31);
+
+        Emp e1 = new Emp();
+        e1.setName("Abdul Kader Babu");
+        e1.setAge(31);
+
+        Emp e2 = new Emp();
+        e2.setName("DIP");
+        e2.setAge(31);
+
+        List<Emp> empList = Arrays.asList(e, e1, e2);
+
+        empList.stream().forEach(consumer);
+
+
     }
 }
