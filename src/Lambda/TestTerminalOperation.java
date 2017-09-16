7405af54b869bbb207a0273e29fbe4d7a1363058
abdul-kader138 <babu313136@gmail.com>
2017-09-16 10:29:50 +0600

    Java 8- New Features Implementation(Terminal Operation)

--- src/Lambda/TestTerminalOperation.java
+++ src/Lambda/TestTerminalOperation.java
@@ -1,10 +1,29 @@
 package Lambda;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestTerminalOperation {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
 
 
+        List<String> obj = new ArrayList<>();
+        obj.add("PPFL");
+        obj.add("PFL");
+        obj.add("PPL");
+        obj.add("PCFL");
+
+        Object[] objArr = obj.stream().toArray();
+        String[] arr =getList(objArr);
+        for (String s : arr) System.out.println(s);
+    }
+
+
+    private static String[] getList(Object[] obj) {
+        String[] val = new String[obj.length];
+        for (int i = 0; i < obj.length; i++) val[i] = (String) obj[i];
+        return val;
     }
 }
