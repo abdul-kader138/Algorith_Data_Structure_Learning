3ccdbdd598b46c4c22eb7ba57cee7483e2def3a2
abdul-kader138 <babu313136@gmail.com>
2017-09-16 11:47:14 +0600

    Java 8- New Features Implementation(Terminal Operation-Collect(toSet,averagingInt))

--- src/Lambda/TestTerminalOperation.java
+++ src/Lambda/TestTerminalOperation.java
@@ -1,9 +1,12 @@
 package Lambda;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.BinaryOperator;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestTerminalOperation {
 
@@ -15,15 +18,15 @@ public class TestTerminalOperation {
         * */
 
 
-
         List<Integer> obj = new ArrayList<>();
         obj.add(1);
         obj.add(2);
         obj.add(4);
+        obj.add(4);
         obj.add(3);
 
         Object[] objArr = obj.stream().toArray();
-        Integer[] arr =getList(objArr);
+        Integer[] arr = getList(objArr);
         for (Integer i : arr) System.out.println(i);
 
 
@@ -36,22 +39,64 @@ public class TestTerminalOperation {
          * */
 
 
-        BinaryOperator<Integer> binaryOperator=new BinaryOperator<Integer>() {
+        BinaryOperator<Integer> binaryOperator = new BinaryOperator<Integer>() {
             @Override
             public Integer apply(Integer integer, Integer integer2) {
-                return (integer+integer2);
+                return (integer + integer2);
             }
         };
 
         /* implement implicitly */
-        int num=obj.stream().reduce(9,(a,b)-> {return (a+b);});
+        int num = obj.stream().reduce(9, (a, b) -> {
+            return (a + b);
+        });
 
         /* implement Explicitly */
-        int num1=obj.stream().reduce(9,binaryOperator);
+        int num1 = obj.stream().reduce(9, binaryOperator);
         System.out.println(num);
         System.out.println(num1);
 
 
+
+
+        /* Example of (Collect-Collectors.toSet())
+          * The Stream.collect terminal operation will convert
+          * the stream into some other container such as a list.
+          * */
+
+
+        Integer[] intArr = {1, 2, 3, 5, 4, 3};
+
+
+        Set<Integer> list = Arrays.asList(intArr).stream().collect(Collectors.toSet());
+        System.out.println(list.size());
+
+
+
+
+
+        /* Example of (Collect-Collectors.averagingInt())
+          * returns a Collector that produces the arithmetic mean
+          * of an integer-valued function applied to the input elements
+          * */
+
+
+        ToIntFunction<Integer> integerToIntFunction = new ToIntFunction<Integer>() {
+            @Override
+            public int applyAsInt(Integer value) {
+                return value;
+            }
+        };
+
+
+            /* implement implicitly */
+        double average = Stream.of(1, 2, 2, 4, 4).collect(Collectors.averagingInt(a -> {
+            return a;
+        }));
+        System.out.println(average);
+
+           /* implement Explicitly */
+        double average1 = Stream.of(1, 2, 2, 4, 4).collect(Collectors.averagingInt(integerToIntFunction));
 
 
     }
