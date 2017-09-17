f10cf6ddf2a16a94964559cc6beb117506d949c0
abdul-kader138 <babu313136@gmail.com>
2017-09-17 12:48:00 +0600

    Java 8- New Features Implementation(Intermediate Operation-filter()

--- src/Lambda/TestIntermediateOperation.java
+++ src/Lambda/TestIntermediateOperation.java
@@ -3,12 +3,34 @@ package Lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class TestIntermediateOperation {
 
 
     public static void main(String[] args) {
+
+
+        List<Emp> empList = new ArrayList<>();
+        Emp emp = new Emp("PPFL", 30);
+        Emp emp1 = new Emp("PPFL1", 30);
+        Emp emp2 = new Emp("PPFL2", 30);
+        Emp emp3 = new Emp("PPL", 40);
+        Emp emp4 = new Emp("PFL", 50);
+        Emp emp5 = new Emp("PFL1", 50);
+        Emp emp6 = new Emp("PCFL", 20);
+
+        empList.add(emp);
+        empList.add(emp1);
+        empList.add(emp2);
+        empList.add(emp3);
+        empList.add(emp4);
+        empList.add(emp5);
+        empList.add(emp6);
+
+
+
 
 
 
@@ -38,6 +60,42 @@ public class TestIntermediateOperation {
         List<String> s = strLIst.parallelStream().distinct().collect(Collectors.toList());
         s.forEach(System.out::println);
 
+
+
+
+
+
+
+
+
+         /* Example of (filter())
+          *  Stream.filter operation will return a new stream
+          *  that contains elements that match its predicate
+          * */
+
+
+        Predicate<Emp> predicate = new Predicate<Emp>() {
+            @Override
+            public boolean test(Emp emp) {
+                if (emp.getAge() > 30) return true;
+                else return false;
+            }
+        };
+
+
+         /* implement Explicitly */
+        List<Emp> empObj = empList.parallelStream().filter(predicate).collect(Collectors.toList());
+        empObj.forEach(System.out::println);
+
+
+
+         /* implement implicitly */
+        List<Emp> empObj1 = empList.parallelStream().filter((a) -> {
+            Emp e = (Emp) a;
+            if (a.getAge() > 30) return true;
+            else return false;
+        }).collect(Collectors.toList());
+        empObj1.forEach(System.out::println);
 
 
     }
