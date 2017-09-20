ff6cab59a526db9b33de037dec0ee132fd184628
abdul-kader138 <babu313136@gmail.com>
2017-09-20 11:51:18 +0600

    Example of InputStream

--- src/Lambda/Annotation_Example/ExampleOfIntStream.java
+++ src/Lambda/Annotation_Example/ExampleOfIntStream.java
@@ -1,5 +1,6 @@
 package Lambda.Annotation_Example;
 
+import java.util.function.IntToDoubleFunction;
 import java.util.stream.IntStream;
 
 public class ExampleOfIntStream {
@@ -11,7 +12,22 @@ public class ExampleOfIntStream {
         /* Get as int  */
         IntStream intStream=IntStream.of(1,3,5,7,8);
         IntStream intStream1=IntStream.of(2,4,6,8,7);
+        IntStream intStream2=IntStream.of(2,4,6,8,7);
         int sum=IntStream.concat(intStream,intStream1).parallel().distinct().max().getAsInt();
         System.out.println(sum);
+
+
+        IntToDoubleFunction intToDoubleFunction=new IntToDoubleFunction() {
+            @Override
+            public double applyAsDouble(int value) {
+                return  (double) value;
+            }
+        };
+        intStream.close();
+
+
+        /* change the int type to Double */
+        Double d=intStream2.parallel().mapToDouble(intToDoubleFunction).max().getAsDouble();
+        System.out.println(d);
     }
 }
