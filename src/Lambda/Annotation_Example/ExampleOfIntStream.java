b88ac0d15f0912fc4fef72551074462bdb06a295
abdul-kader138 <babu313136@gmail.com>
2017-09-20 12:11:35 +0600

    Example of InputStream

--- src/Lambda/Annotation_Example/ExampleOfIntStream.java
+++ src/Lambda/Annotation_Example/ExampleOfIntStream.java
@@ -1,7 +1,12 @@
 package Lambda.Annotation_Example;
 
+import java.util.List;
+import java.util.Random;
+import java.util.function.IntSupplier;
 import java.util.function.IntToDoubleFunction;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ExampleOfIntStream {
 
@@ -29,5 +34,21 @@ public class ExampleOfIntStream {
         /* change the int type to Double */
         Double d=intStream2.parallel().mapToDouble(intToDoubleFunction).max().getAsDouble();
         System.out.println(d);
+
+
+        /* boxed()- Used to convert primitive type stream to Object type stream */
+
+
+        IntSupplier intSupplier=new IntSupplier() {
+            Random random=new Random();
+            @Override
+            public int getAsInt() {
+                int num= random.nextInt(100);
+                return num;
+            }
+        };
+
+        List<Integer> stream= IntStream.rangeClosed(1,10).boxed().collect(Collectors.toList());
+        stream.forEach(System.out::println);
     }
 }
