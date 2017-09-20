e4cc3915ae1f933f127913703417e05ef174d25b
abdul-kader138 <babu313136@gmail.com>
2017-09-20 11:46:57 +0600

    Example of InputStream

--- src/Lambda/Annotation_Example/ExampleOfIntStream.java
+++ src/Lambda/Annotation_Example/ExampleOfIntStream.java
@@ -1,7 +1,17 @@
 package Lambda.Annotation_Example;
 
-/**
- * Created by a.kader on 9/20/2017.
- */
+import java.util.stream.IntStream;
+
 public class ExampleOfIntStream {
+
+
+    public static void main(String[] args){
+
+
+        /* Get as int  */
+        IntStream intStream=IntStream.of(1,3,5,7,8);
+        IntStream intStream1=IntStream.of(2,4,6,8,7);
+        int sum=IntStream.concat(intStream,intStream1).parallel().distinct().max().getAsInt();
+        System.out.println(sum);
+    }
 }
